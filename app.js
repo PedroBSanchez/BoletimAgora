39d65b2cfd9fbb82e53a58e4cb0d4b79f2642fa0
Lucas Campos dos Santos <lucascamposmga@gmail.com>
2021-06-16 00:16:52 -0300

    Segundo Commit

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-app.use((req, res, next) =>{
-    res.status(200).send({
-        mensagem: 'OK, deu certo'
-    });
+//Chamar Rotas
+const rotaEscolas = require('./routes/escolas.js');
+
+//Configurações e melhorias
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false})); //Apenas dados simples
+app.use(bodyParser.json()); //json de entrada no body
+
+//Rotas
+app.use('/escolas', rotaEscolas);
+
+//Quando não encontra rota entra aqui
+app.use((req, res, next)=>{
+    const erro = new Error('Não Encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    })
 });
 
 module.exports = app;
---
+++ routes/escolas.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('../mysql').pool;
+
+
+router.get('/',(req, res, next) =>{
+    mysql.getConnection((error, conn)=>{
+        if(error){return res.status(500).send({error: error}) }
+        conn.query(
+            'SELECT * FROM ESCOLAS',
+            (error, resultado, field)=>{
+                conn.release();
+                if(error){return res.status(500).send({error: error}) }
+                res.status(200).send({escolas: resultado});
+            }
+        )
+    });
+
+        
+});
+
+router.post('/',(req, res, next) =>{
+    res.status(201).send({
+        mensagem: 'Usando o POST - Rota de Escolas'
+    });
+});
+
+router.get('/:id_escolas',(req, res, next) =>{
+    const id = req.params.id_escolas;
+    res.status(200).send({
+        mensagem: 'Usando o GET de produto específico - Rota de Escolas',
+        id: id
+    });
+});
+
+module.exports = router;
